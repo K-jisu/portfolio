a6083812b75a20b5033b6f1d74e8fe1260f911ae
K-jisu <wltn7star@naver.com>
2026-02-13 23:09:33 +0900

    feat : 모달 store 추가

---
+++ shared/store/modalStore.ts
@@ -0,0 +1,14 @@
+import { create } from 'zustand';
+type ModalStore = {
+  isModalOpen: boolean;
+  selectProject: number | null;
+  openModal: (id: number) => void;
+  closeModal: () => void;
+};
+
+const useModalStore = create<ModalStore>((set) => ({
+  isModalOpen: false,
+  selectProject: null,
+  openModal: (id) => set({ isModalOpen: true, selectProject: id }),
+  closeModal: () => set({ isModalOpen: false, selectProject: null }),
+}));
