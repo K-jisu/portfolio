7a3b5570a9916261b1b6b6de405571f6999115fd
K-jisu <wltn7star@naver.com>
2026-02-14 20:19:47 +0900

    feat : PROJECTS 파일 분리

--- widgets/Main/ui/ProjectSection.tsx
+++ widgets/Main/ui/ProjectSection.tsx
@@ -1,46 +1,10 @@
 'use client';
+import { PROJECTS } from '@/features/main/model/projects';
 import ProjectCard from '@/features/main/ui/ProjectCard';
 import ProjectModal from '@/features/main/ui/ProjectModal';
 import { useState } from 'react';
 
-export const PROJECTS = [
-  {
-    id: 1,
-    title: '메이드존',
-    skills: ['nextjs', 'typescript', 'tailwind', 'framer'],
-    period: '2025.10 ~ 2025.12',
-    description:
-      '체계적인 학습 관리 시스템과 전문 멘토링으로 학생들의 자기주도학습을 지원하는 프리미엄 독서실',
-    imageUrl: 'madezone_desktop',
-  },
-  {
-    id: 2,
-    title: '루나폴',
-    skills: ['react', 'vite', 'typescript', 'tailwind', 'i18n'],
-    period: '2025.10 ~ 2025.11',
-    description:
-      'AI 음성 내레이션으로 공간의 세계관을 따라 걷듯 체험하게 하는 몰입형 오디오 가이드 서비스',
-    imageUrl: 'lunafall_mobile',
-  },
-  {
-    id: 3,
-    title: '당재재',
-    skills: ['nextjs', 'typescript', 'tailwind', 'framer'],
-    period: '2025.11 ~ 2025.12',
-    description:
-      '체계적인 학습 관리 시스템과 전문 멘토링으로 학생들의 자기주도학습을 지원하는 프리미엄 독서실',
-    imageUrl: 'dangjaejae_desktop',
-  },
-  {
-    id: 4,
-    title: 'UUno',
-    skills: ['nextjs', 'typescript', 'tailwind', 'framer'],
-    period: '2025.11 ~ 2025.12',
-    description:
-      '체계적인 학습 관리 시스템과 전문 멘토링으로 학생들의 자기주도학습을 지원하는 프리미엄 독서실',
-    imageUrl: 'dangjaejae_desktop',
-  },
-];
+
 
 const ProjectSection = () => {
   const [selectProject, setSelectProject] = useState<number | null>(null);
