9a25e368ef916a48139c1a60ae1dace7ace3bc6d
K-jisu <wltn7star@naver.com>
2026-02-20 23:52:08 +0900

    feat : 모달 내용 수정

--- features/main/ui/ProjectModal.tsx
+++ features/main/ui/ProjectModal.tsx
@@ -9,6 +9,10 @@ import {
 } from '@/shared/ui/carousel';
 import { useEffect } from 'react';
 import { PROJECTS } from '../model/projects';
+import Image from 'next/image';
+import { Badge } from '@/shared/ui/badge';
+import Link from 'next/link';
+import { Globe, X } from 'lucide-react';
 
 type ProjectModalProps = {
   id: number;
@@ -19,7 +23,20 @@ const ProjectModal = ({ id, onClose }: ProjectModalProps) => {
   const project = PROJECTS.find((project) => project.id === id);
   if (!project) return null;
 
-  const { title, skills, period, description, imageUrl } = project;
+  const {
+    title,
+    skills,
+    period,
+    description,
+    imageUrl,
+    company,
+    contibution,
+    role,
+    members,
+    url,
+    github,
+    details,
+  } = project;
   const projectImages = [imageUrl];
 
   // 백그라운드 스크롤 방지
@@ -34,8 +51,14 @@ const ProjectModal = ({ id, onClose }: ProjectModalProps) => {
 
   return (
     <>
-      <div className="fixed inset-0 z-50 flex top-20 items-center justify-center bg-black/60 p-4 md:p-10 backdrop-blur-2xl">
-        <div className="glass-modal relative flex max-h-[90vh] w-full max-w-6xl flex-col overflow-hidden rounded-xl shadow-2xl">
+      <div
+        className="fixed inset-0 z-50 flex top-20 items-center justify-center bg-black/60 p-4 md:p-10 backdrop-blur-2xl"
+        onClick={onClose}
+      >
+        <div
+          className="glass-modal relative flex max-h-[90vh] w-full max-w-6xl flex-col overflow-hidden rounded-xl shadow-2xl"
+          onClick={(e) => e.stopPropagation()}
+        >
           <header className="flex items-center justify-between border-b border-[#224249] px-8 py-6">
             <div className="flex items-center gap-3">
               <div className="neon-glow size-3 rounded-full bg-[#0dccf2]" />
@@ -44,12 +67,12 @@ const ProjectModal = ({ id, onClose }: ProjectModalProps) => {
               </h1>
             </div>
             <button
-              className="group flex size-10 items-center justify-center rounded-lg border border-transparent bg-[#224249] text-white transition-all hover:border-[#0dccf2]/50 hover:bg-[#0dccf2]/20"
+              className="group flex size-10 items-center justify-center rounded-lg border border-transparent bg-[#224249] text-white transition-all hover:border-[#0dccf2]/50 hover:bg-[#0dccf2]/20 cursor-pointer"
               aria-label="Close modal"
               onClick={onClose}
             >
-              <span className="material-symbols-outlined text-xl transition-transform group-hover:rotate-90">
-                close
+              <span className="material-symbols-outlined text-xl ">
+                <X />
               </span>
             </button>
           </header>
@@ -90,98 +113,123 @@ const ProjectModal = ({ id, onClose }: ProjectModalProps) => {
                 <section>
                   <h3 className="mb-4 flex items-center gap-2 text-sm font-bold tracking-[0.2em] text-[#0dccf2] uppercase">
                     <span className="material-symbols-outlined text-lg">
-                      info
+                      서비스
                     </span>
-                    Overview
                   </h3>
-                  <p className="text-lg leading-relaxed text-gray-300">
+                  <p className="text-lg leading-relaxed text-white whitespace-normal break-keep">
                     {description}
                   </p>
                   <p className="mt-3 text-sm text-gray-400">{period}</p>
                 </section>
 
                 <section>
-                  <h3 className="mb-6 flex items-center gap-2 text-sm font-bold tracking-[0.2em] text-[#0dccf2] uppercase">
+                  <h3 className="mb-4 flex items-center gap-2 text-sm font-bold tracking-[0.2em] text-[#0dccf2] uppercase">
                     <span className="material-symbols-outlined text-lg">
-                      featured_play_list
+                      담당 역할
                     </span>
-                    Key Features
                   </h3>
-                  <div className="grid grid-cols-1 gap-4 md:grid-cols-2"></div>
+                  <p className="text-lg leading-relaxed text-white whitespace-normal break-keep">
+                    {role}
+                  </p>
+                </section>
+
+                <section>
+                  <h3 className="mb-4 flex items-center gap-2 text-sm font-bold tracking-[0.2em] text-[#0dccf2] uppercase">
+                    <span className="material-symbols-outlined text-lg">
+                      상세 내용
+                    </span>
+                  </h3>
+                  <div className="flex flex-col gap-2">
+                    {details.map((detail, index) => (
+                      <div className="flex flex-row gap-1">
+                        -
+                        <p
+                          className="text-lg leading-relaxed text-white whitespace-normal break-keep"
+                          key={index}
+                        >
+                          {detail}
+                        </p>
+                      </div>
+                    ))}
+                  </div>
                 </section>
               </div>
 
               <div className="space-y-10 lg:col-span-5">
                 <section>
-                  <h3 className="mb-6 text-sm font-bold tracking-[0.2em] text-[#0dccf2] uppercase">
-                    Technologies
+                  <h3 className="mb-4 text-lg font-bold tracking-[0.2em] text-[#0dccf2] uppercase">
+                    기술 스택
                   </h3>
                   <div className="flex flex-wrap gap-3">
-                    {skills.map((skill) => (
-                      <div
-                        key={skill}
-                        className="flex items-center gap-2 rounded border border-[#0dccf2]/20 bg-[#0dccf2]/10 px-3 py-2"
+                    {skills.map((skill, index) => (
+                      <Badge
+                        key={index}
+                        className="text-[10px] px-2 py-1 rounded bg-white/5 text-[#0dccf2] border border-[#0dccf2]/20"
                       >
-                        <span className="material-symbols-outlined text-sm text-[#0dccf2]">
-                          memory
-                        </span>
-                        <span className="text-xs font-bold uppercase">
-                          {skill}
-                        </span>
-                      </div>
+                        <Image
+                          alt={skill}
+                          src={`/${skill}.png`}
+                          width={20}
+                          height={20}
+                          className="rounded-sm"
+                        />
+                        {/* {skill} */}
+                      </Badge>
                     ))}
                   </div>
                 </section>
 
                 <section>
-                  <h3 className="mb-4 text-sm font-bold tracking-[0.2em] text-[#0dccf2] uppercase">
-                    My Role
+                  <h3 className="mb-4 text-lg font-bold tracking-[0.2em] text-[#0dccf2] uppercase">
+                    연계/소속회사
                   </h3>
-                  <ul className="space-y-4">
-                    <li className="flex items-start gap-3">
-                      <span className="material-symbols-outlined mt-1 text-[#0dccf2]">
-                        check_circle
-                      </span>
-                      <p className="text-sm text-gray-300">
-                        Architected the frontend state management system using
-                        Redux Toolkit and RTK Query.
-                      </p>
-                    </li>
-                    <li className="flex items-start gap-3">
-                      <span className="material-symbols-outlined mt-1 text-[#0dccf2]">
-                        check_circle
-                      </span>
-                      <p className="text-sm text-gray-300">
-                        Implemented a custom SVG charting library to handle
-                        high-density data visualizations.
-                      </p>
-                    </li>
-                    <li className="flex items-start gap-3">
-                      <span className="material-symbols-outlined mt-1 text-[#0dccf2]">
-                        check_circle
-                      </span>
-                      <p className="text-sm text-gray-300">
-                        Led the design team in creating the glassmorphism UI
-                        system and component library.
-                      </p>
-                    </li>
-                  </ul>
+                  <p className="text-md text-gray-400">{company}</p>
+                </section>
+
+                <section>
+                  <h3 className="mb-4 text-lg font-bold tracking-[0.2em] text-[#0dccf2] uppercase">
+                    구성원
+                  </h3>
+                  <p className="text-md text-gray-400">{members}</p>
+                </section>
+
+                <section>
+                  <h3 className="mb-4 text-lg font-bold tracking-[0.2em] text-[#0dccf2] uppercase">
+                    기여도
+                  </h3>
+                  <p className="text-md text-gray-400">{contibution}</p>
                 </section>
               </div>
             </div>
           </div>
 
           <footer className="flex flex-col items-center justify-end gap-4 border-t border-[#224249] bg-[#101f22]/50 p-6 sm:flex-row">
-            <button className="flex w-full items-center justify-center gap-2 rounded-lg border border-[#0dccf2]/50 px-8 py-3 text-sm font-bold tracking-wider text-[#0dccf2] uppercase transition-all hover:bg-[#0dccf2]/10 sm:w-auto">
-              <span className="material-symbols-outlined text-xl">code</span>
-              GitHub Repo
-            </button>
-            <button className="neon-glow flex w-full items-center justify-center gap-2 rounded-lg bg-[#0dccf2] px-8 py-3 text-sm font-bold tracking-wider text-[#101f22] uppercase transition-all hover:brightness-110 sm:w-auto">
-              <span className="material-symbols-outlined text-xl">
-                open_in_new
-              </span>
-              Live Demo
-            </button>
+            {github && (
+              <Link
+                href={url}
+                target="_blank"
+                className="neon-glow flex w-full items-center justify-center rounded-lg bg-[#0dccf2] px-8 py-3 text-sm font-bold  text-[#101f22] transition-all hover:brightness-110 sm:w-auto"
+              >
+                <div className="flex flex-row">
+                  <Image
+                    alt="github"
+                    src={`/github.png`}
+                    width={24}
+                    height={24}
+                    className="mr-2 inline-block"
+                  />
+                  GO TO GITHUB
+                </div>
+              </Link>
+            )}
+            <Link
+              href={url}
+              target="_blank"
+              className="flex w-full items-center justify-center gap-2 rounded-lg border border-[#0dccf2]/50 px-8 py-3 text-sm font-bold tracking-wider text-[#0dccf2] uppercase transition-all hover:bg-[#0dccf2]/10 sm:w-auto"
+            >
+              <Globe className="mr-2 inline-block" />
+              GO TO WEBSITE
+            </Link>
           </footer>
         </div>
       </div>
